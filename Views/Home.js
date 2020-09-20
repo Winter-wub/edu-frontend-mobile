66af9264ee8cb9a25411180c2af0acb00446f513
Winter-wub <prachayawut.s@gmail.com>
2020-09-20 21:36:58 +0700

    🔧 add loading

--- Views/Home.js
+++ Views/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView } from "react-native";
 import { Col, Grid, Row } from "react-native-easy-grid";
 import Section from "../Components/Section";
@@ -6,106 +6,114 @@ import Container from "../Components/ViewContainer";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import config from "../config.json";
-
-const mockVideo = [
-  {
-    id: "1",
-    thumbnail:
-      "https://cdn.iconscout.com/icon/free/png-256/no-image-1771002-1505134.png",
-    title: "Test Content 1",
-    subTitle: "Test Content Subtitle 1",
-  },
-  {
-    id: "2",
-    thumbnail:
-      "https://cdn.iconscout.com/icon/free/png-256/no-image-1771002-1505134.png",
-    title: "Test Content 2",
-    subTitle: "Test Content Subtitle 2",
-  },
-  {
-    id: "3",
-    thumbnail:
-      "https://cdn.iconscout.com/icon/free/png-256/no-image-1771002-1505134.png",
-    title: "Test Content 3",
-    subTitle: "Test Content Subtitle 3",
-  },
-];
-
-const mockEssay = [
-  {
-    id: "1",
-    thumbnail:
-      "https://cdn.iconscout.com/icon/free/png-256/no-image-1771002-1505134.png",
-    title: "Test Content 1",
-    subTitle: "Test Content Subtitle 1",
-  },
-  {
-    id: "2",
-    thumbnail:
-      "https://cdn.iconscout.com/icon/free/png-256/no-image-1771002-1505134.png",
-    title: "Test Content 2",
-    subTitle: "Test Content Subtitle 2",
-  },
-  {
-    id: "3",
-    thumbnail:
-      "https://cdn.iconscout.com/icon/free/png-256/no-image-1771002-1505134.png",
-    title: "Test Content 3",
-    subTitle: "Test Content Subtitle 3",
-  },
-];
-
-const mockVocab = [
-  {
-    id: "1",
-    thumbnail:
-      "https://cdn.iconscout.com/icon/free/png-256/no-image-1771002-1505134.png",
-    title: "Test Content 1",
-    subTitle: "Test Content Subtitle 1",
-  },
-  {
-    id: "2",
-    thumbnail:
-      "https://cdn.iconscout.com/icon/free/png-256/no-image-1771002-1505134.png",
-    title: "Test Content 2",
-    subTitle: "Test Content Subtitle 2",
-  },
-  {
-    id: "3",
-    thumbnail:
-      "https://cdn.iconscout.com/icon/free/png-256/no-image-1771002-1505134.png",
-    title: "Test Content 3",
-    subTitle: "Test Content Subtitle 3",
-  },
-];
+import { Text } from "react-native-elements";
+import { firestore } from "../Utils/firebase";
+import { AppLoading } from "expo";
 
 export default function Home() {
-  return (
+  const [video, setVideo] = useState([]);
+  const [essay, setEssay] = useState([]);
+  const [vocab, setVocab] = useState([]);
+  const [load, setLoad] = useState(true);
+
+  const loadingNews = async () => {
+    const appConfigHomeRef = await firestore
+      .collection(config.collections.app_config)
+      .doc("home")
+      .get();
+
+    const videosRef = await Promise.all(
+      appConfigHomeRef.data().videos.map(async (item) => await item.get())
+    );
+
+    setVideo(
+      videosRef.map((ref) => ({
+        id: ref.id,
+        path: `/content/${ref.id}`,
+        ...ref.data(),
+      }))
+    );
+
+    const essayRef = await Promise.all(
+      appConfigHomeRef.data().essays.map(async (item) => await item.get())
+    );
+
+    setEssay(
+      essayRef.map((ref) => ({
+        id: ref.id,
+        path: `/content/${ref.id}`,
+        ...ref.data(),
+      }))
+    );
+
+    const vocabRef = await Promise.all(
+      appConfigHomeRef.data().vocab.map(async (item) => await item.get())
+    );
+
+    setVocab(
+      vocabRef.map((ref) => ({
+        id: ref.id,
+        path: `/content/${ref.id}`,
+        ...ref.data(),
+      }))
+    );
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await loadingNews();
+      } finally {
+        setLoad(false);
+      }
+    })();
+  }, []);
+
+  return load ? (
+    <AppLoading />
+  ) : (
     <Container>
       <Header title={config.app.title} />
       <ScrollView>
+        <Text
+          h4
+          style={{
+            alignSelf: "center",
+            margin: 5,
+            padding: 5,
+          }}
+        >
+          What&#39;s new ?
+        </Text>
         <Grid>
           <Row>
             <Col>
               <Section
                 title="Videos"
-                item={mockVideo.map((item) => ({
-                  ...item,
-                  path: `/content/${item.id}`,
-                }))}
-                path="/course/1"
+                item={video}
+                path="/course/videos"
                 type="video"
               />
             </Col>
           </Row>
           <Row>
             <Col>
-              <Section title="Essay" item={mockEssay} />
+              <Section
+                title="Essay"
+                item={essay}
+                path="/course/essay"
+                type="essay"
+              />
             </Col>
           </Row>
           <Row>
             <Col>
-              <Section title="Vocabulary" item={mockVocab} />
+              <Section
+                title="Vocabulary"
+                item={vocab}
+                path="/course/vocab"
+                type="vocab"
+              />
             </Col>
           </Row>
         </Grid>
