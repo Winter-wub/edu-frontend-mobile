32eb2325f2e4ea33b45272ca8a1dccfdb55e60c7
Winter-wub <prachayawut.s@gmail.com>
2020-09-23 01:28:47 +0700

    🔧 add question list

--- Views/Quiz.js
+++ Views/Quiz.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Components/Header";
 import Container from "../Components/ViewContainer";
 import Footer from "../Components/Footer";
 import { ScrollView } from "react-native";
+import { firestore } from "../Utils/firebase";
+import config from "../config.json";
+import CardItem from "../Components/CardItem";
 
 export default function Quiz() {
+  const [quiz, setQuiz] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const quizRef = await firestore.collection(config.collections.quiz).get();
+      const quiz = quizRef.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setQuiz(quiz);
+    })();
+  }, []);
+
+  const onPressItem = (id) => {};
+
   return (
     <Container>
       <Header title="Quiz" />
-      <ScrollView></ScrollView>
+      <ScrollView>
+        {quiz.map((item) => (
+          <CardItem
+            key={item.id}
+            title={item?.title}
+            subTitle={item?.subTitle}
+            onPress={() => onPressItem(item.id)}
+            thumbnail={item.thumbnail}
+          />
+        ))}
+      </ScrollView>
       <Footer />
     </Container>
   );
