e72f14e2c949a1fa6c1dcca7f6079babdd94ae6e
Winter-wub <prachayawut.s@gmail.com>
2020-09-24 00:09:51 +0700

    🔧 add development support iphone emulator

--- Utils/firebase.js
+++ Utils/firebase.js
@@ -2,16 +2,23 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import config from "../config.json";
-import { YellowBox } from "react-native";
+import { YellowBox, Platform } from "react-native";
 export const firebaseApp = firebase.initializeApp(config.firebase);
 export const firestore = firebaseApp.firestore();
 export const auth = firebaseApp.auth();
 
 if (process.env.NODE_ENV === "development") {
-  firestore.settings({
-    host: "10.0.2.2:8080",
-    ssl: false,
-  });
+  if (Platform.OS === "android") {
+    firestore.settings({
+      host: "10.0.2.2:8080",
+      ssl: false,
+    });
+  } else {
+    firestore.settings({
+      host: "localhost:8080",
+      ssl: false,
+    });
+  }
 
   YellowBox.ignoreWarnings(["Setting a timer"]);
   const _console = { ...console };
