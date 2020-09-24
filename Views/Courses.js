b3e80f978bc7fd48e278cf98b1d4dd4561361434
Winter-wub <prachayawut.s@gmail.com>
2020-09-24 14:48:46 +0700

    🔧add search by type

--- Views/Courses.js
+++ Views/Courses.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-native";
-import { ScrollView, ActivityIndicator } from "react-native";
+import { ScrollView, ActivityIndicator, View } from "react-native";
 import Header from "../Components/Header";
 import Container from "../Components/ViewContainer";
 import CardItem from "../Components/CardItem";
 import { firestore } from "../Utils/firebase";
 import config from "../config.json";
-import { Overlay } from "react-native-elements";
+import { Button, Icon, ListItem, Overlay, Text } from "react-native-elements";
 
 export default function Courses() {
   const history = useHistory();
@@ -14,6 +14,9 @@ export default function Courses() {
   const [items, setItems] = useState([]);
   const [load, setLoad] = useState(true);
   const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
+  const [typeList, setTypeList] = useState([]);
+  const [toggleSearch, setToggleSearch] = useState(false);
 
   const onPressItem = (link) => {
     history.push(`/content/${link}`, { type: id });
@@ -28,29 +31,101 @@ export default function Courses() {
 
         const docData = await docRef.get();
         setTitle(docData.data().title);
+        if (type !== "") {
+          console.log("search by type");
+          const coursesRef = await docRef
+            .collection("courses")
+            .where("type", "==", type)
+            .get();
+          const data = await Promise.all(
+            coursesRef.docs.map(async (doc) => ({
+              id: doc.id,
+              ...(await doc.data()),
+              link: doc.id,
+            }))
+          );
+          setItems(data);
+        } else {
+          const coursesRef = await docRef.collection("courses").get();
+          const data = await Promise.all(
+            coursesRef.docs.map(async (doc) => ({
+              id: doc.id,
+              ...(await doc.data()),
+              link: doc.id,
+            }))
+          );
+          setItems(data);
+        }
 
-        const coursesRef = await docRef.collection("courses").get();
-        const data = await Promise.all(
-          coursesRef.docs.map(async (doc) => ({
-            id: doc.id,
-            ...(await doc.data()),
-            link: doc.id,
-          }))
-        );
-        setItems(data);
+        const appConfig = (
+          await firestore
+            .collection(config.collections.app_config)
+            .doc("categories")
+            .get()
+        ).data();
+        if (id === "videos") {
+          setTypeList(appConfig.videos_type);
+        } else if (id === "essays") {
+          setTypeList(appConfig.essays_type);
+        } else if (id === "vocab") {
+          setTypeList(appConfig.vocab_type);
+        } else {
+          setTypeList([]);
+        }
       } catch (e) {
         console.log(e);
       } finally {
         setLoad(false);
       }
     })();
-  }, []);
+  }, [type]);
   return (
     <Container>
       <Overlay isVisible={load}>
         <ActivityIndicator />
       </Overlay>
+      <Overlay
+        isVisible={toggleSearch}
+        onBackdropPress={() => setToggleSearch(false)}
+      >
+        <View style={{ display: "flex", justifyContent: "center" }}>
+          {typeList.map((item, id) => (
+            <ListItem
+              bottomDivider
+              key={id}
+              onPress={() => {
+                setType(item);
+                setToggleSearch(false);
+              }}
+            >
+              <ListItem.Title>{item}</ListItem.Title>
+            </ListItem>
+          )) ?? []}
+        </View>
+      </Overlay>
       <Header title={title} goBack />
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: 15,
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Button
+          type="clear"
+          icon={<Icon name="search" type="material" />}
+          onPress={() => setToggleSearch(true)}
+        />
+        <Text>{type === "" ? "Search by Type" : type}</Text>
+        <Button
+          type="clear"
+          title="clear"
+          style={{ marginRight: 0 }}
+          onPress={() => setType("")}
+        />
+      </View>
       <ScrollView>
         {items.map((item) => (
           <CardItem
