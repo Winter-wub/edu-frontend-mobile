6ec2e6e4d1a22ad853b3d5a051cb78619b344019
Winter-wub <prachayawut.s@gmail.com>
2020-10-17 20:58:32 +0700

    🔧 add update app

--- App.js
+++ App.js
@@ -14,90 +14,106 @@ import DrawerContext from "./Contexts/Drawer";
 import useDrawer from "./Hooks/Drawer";
 import SideMenu from "react-native-side-menu";
 import SidebarMenu from "./Components/SidebarMenu";
-import { AppLoading } from "expo";
 import Forget from "./Views/Forget";
 import MyScore from "./Views/MyScore";
 import Stack from "react-router-native-stack";
 import useInitApp from "./Hooks/InitialApp";
 import UpdateDialog from "./Components/UpdateDialog";
 import theme from "./theme";
+import { AppLoading } from "expo";
 
 export default function App() {
   const { open, setOpen } = useDrawer();
-  const { isInitialing: load, hasUpdate, setConfirmUpdate } = useInitApp();
-
-  if (load) {
-    return <AppLoading />;
-  }
+  const {
+    isInitialing: load,
+    initResource,
+    setLoad,
+    hasUpdate,
+    setConfirmUpdate,
+  } = useInitApp();
 
   const toggleDrawer = () => {
     setOpen(!open);
   };
 
-  return (
-    <ThemeProvider theme={theme}>
-      <NativeRouter>
-        <DrawerContext.Provider value={toggleDrawer}>
-          <SideMenu
-            menuPosition="right"
-            isOpen={open}
-            menu={<SidebarMenu />}
-            disableGestures
-          >
-            <Stack gestureEnabled={false}>
-              <Route
-                exact
-                path="/"
-                animationType="slide-horizontal"
-                component={Home}
-              />
-              <Route path="/login" animationType="none" component={Login} />
-              <Route
-                path="/register"
-                animationType="slide-vertical"
-                component={Register}
-              />
-              <Route
-                path="/content/:id/:type"
-                animationType="slide-vertical"
-                component={Content}
-              />
-              <Route
-                path="/course/:id"
-                animationType="slide-vertical"
-                component={Courses}
-              />
-              <Route
-                path="/quiz/:id"
-                animationType="slide-vertical"
-                component={Question}
-              />
-              <Route
-                exact
-                path="/quiz"
-                animationType="slide-horizontal"
-                component={Quiz}
-              />
-              <Route path="/about" component={About} />
-              <Route path="/profile" component={Profile} />
-              <Route
-                path="/forget"
-                animationType="slide-vertical"
-                component={Forget}
-              />
-              <Route
-                path="/myscore"
-                animationType="slide-vertical"
-                component={MyScore}
-              />
-            </Stack>
-          </SideMenu>
-        </DrawerContext.Provider>
-      </NativeRouter>
-      <UpdateDialog
-        showUpdate={hasUpdate}
-        handleShowUpdate={setConfirmUpdate}
+  if (load) {
+    return (
+      <AppLoading
+        startAsync={initResource}
+        onFinish={async () => {
+          setLoad(false);
+        }}
+        onError={(error) => {
+          console.warn(error);
+        }}
       />
-    </ThemeProvider>
-  );
+    );
+  } else {
+    return (
+      <ThemeProvider theme={theme}>
+        <NativeRouter>
+          <DrawerContext.Provider value={toggleDrawer}>
+            <SideMenu
+              menuPosition="right"
+              isOpen={open}
+              menu={<SidebarMenu />}
+              disableGestures
+            >
+              <Stack gestureEnabled={false}>
+                <Route
+                  exact
+                  path="/"
+                  animationType="slide-horizontal"
+                  component={Home}
+                />
+                <Route path="/login" animationType="none" component={Login} />
+                <Route
+                  path="/register"
+                  animationType="slide-vertical"
+                  component={Register}
+                />
+                <Route
+                  path="/content/:id/:type"
+                  animationType="slide-vertical"
+                  component={Content}
+                />
+                <Route
+                  path="/course/:id"
+                  animationType="slide-vertical"
+                  component={Courses}
+                />
+                <Route
+                  path="/quiz/:id"
+                  animationType="slide-vertical"
+                  component={Question}
+                />
+                <Route
+                  exact
+                  path="/quiz"
+                  animationType="slide-horizontal"
+                  component={Quiz}
+                />
+                <Route path="/about" component={About} />
+                <Route path="/profile" component={Profile} />
+                <Route
+                  path="/forget"
+                  animationType="slide-vertical"
+                  component={Forget}
+                />
+                <Route
+                  path="/myscore"
+                  animationType="slide-vertical"
+                  component={MyScore}
+                />
+              </Stack>
+            </SideMenu>
+          </DrawerContext.Provider>
+        </NativeRouter>
+        <UpdateDialog
+          showUpdate={hasUpdate}
+          handleShowUpdate={setConfirmUpdate}
+        />
+      </ThemeProvider>
+    );
+  }
 }
